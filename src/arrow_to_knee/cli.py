3933e5b452e5ff01d37670da803422699eef5167
Angus Hollands <goosey15@gmail.com>
2025-06-25 12:17:35 +0100

    fix: improve examples

--- src/arrow_to_knee/cli.py
+++ src/arrow_to_knee/cli.py
@@ -1,4 +1,9 @@
-def remove_arrow():
-    print("It's stuck! Can't remove")
-    raise SystemExit(1)
+# This is an optional dependency, so importing
+# _this_ package will fail if you don't have it
+from rich import print as rprint
 
+
+def remove_arrow():
+    # Uses the rich library for formatting
+    rprint("[italic red]It's stuck! Can't remove[/italic red]")
+    raise SystemExit(1)
--- src/arrow_to_knee/plugin.py
+++ src/arrow_to_knee/plugin.py
@@ -1,2 +1,7 @@
+import numpy as np
+
+
 def plugin():
-    print("Ow!!! My kneeeeeeeeeeeeeeeee. Ow.")
+    n = np.random.randint(5, 32)
+    letters = "e" * n
+    print(f"Ow!!! My kn{letters}. Ow.")
